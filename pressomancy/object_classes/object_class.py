3f6c2c8ee976c0d11d16a8c973dd8fa055150152
stekajack <avantardejack@gmail.com>
2024-11-04 19:32:50 +0100

    Simulation_Object reimagined as a metaclass to  enforce attributes in derived classes

--- pressomancy/object_classes/object_class.py
+++ pressomancy/object_classes/object_class.py
@@ -1,22 +1,58 @@
-import espressomd
-from pressomancy.helper_functions import RoutineWithArgs
+from pressomancy.helper_functions import RoutineWithArgs, PartDictSafe
 
-class Simulation_Object():
+def generic_type_exception(name, attribute_name, expected_type):
+    raise NotImplementedError(
+                    f"The class attribute '{attribute_name}' is required in '{name}' but not defined. "
+                    f"Please define '{attribute_name}' as a '{expected_type.__name__}' in your subclass."
+                )
 
-    '''
-    Generic object class from which every object implementation should inherit. Containes common interface methods for any object to mimic virtual function behaviour.
-    '''
-    who_am_i=None
+class Simulation_Object(type):
+    """
+    Metaclass for simulation objects. Enforces required attributes and provides shared methods.
+    """
+
+    def __init__(cls, name, bases, class_dict):
+        super().__init__(name, bases, class_dict)
+        # dict of required attributes
+        required_attributes = {
+            "numInstances": int,
+            "n_parts": int,
+            "size": float,
+            "part_types": PartDictSafe
+        }
+        for attr, expected_type in required_attributes.items():
+            if not hasattr(cls, attr):
+                generic_type_exception(name, attr, expected_type)
+            # Check if attribute is of the correct type
+            elif not isinstance(getattr(cls, attr), expected_type):
+                generic_type_exception(name, attr, expected_type)
     
+    def __call__(cls, *args, **kwargs):
+        # Create a new instance of the class
+        instance = super().__call__(*args, **kwargs)
+        required_attributes = ["who_am_i",]
+        for attr in required_attributes:
+            # Check for required instance attribute `who_am_i`
+            if not hasattr(instance, attr):
+                raise NotImplementedError(
+                    "The instance attribute 'who_am_i' is required but not defined. "
+                    "Please define it in the subclass's __init__ method."
+                )
+        return instance
+
+    # Shared method for equality comparison based on `who_am_i`
     def __eq__(self, other):
-        # Ensure the same type and then compare based on `who_am_i`
-        return isinstance(other, self.__class__) and self.who_am_i == other.who_am_i
+        if not isinstance(other, self.__class__):
+            return False
+        return getattr(self, "who_am_i", None) == getattr(other, "who_am_i", None)
 
+    # Shared method for hashing based on `who_am_i`
     def __hash__(self):
-        # Hash using class type and `who_am_i`
-        return hash((self.__class__, self.who_am_i))
-    
-    build_function=RoutineWithArgs()
+        return hash((self.__class__, getattr(self, "who_am_i", None)))
 
-    def set_object(self,  pos, ori):
-        raise NotImplementedError()
+    # Placeholder for build function
+    build_function = RoutineWithArgs()
+
+    # Shared method for setting object attributes (meant to be overridden if necessary)
+    def set_object(self, pos, ori):
+        raise NotImplementedError("The 'set_object' method must be implemented in subclasses.")
